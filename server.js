27602f880c430ee385c11d96e5b46f987af17d62
circulomilitarec <sistemas@circulomilitar.ec>
2024-06-26 12:14:21 -0500

    Database set

--- server.js
+++ server.js
@@ -4,8 +4,15 @@
 
 //mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const { Pool } = require('pg');
 const express = require('express');
 const app = express();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Utiliza esto para conexiones SSL. Modifica según sea necesario.
+  }
+})
 const port = process.env.PORT || 3000;
 
 // Define a route for the root URL (/)
@@ -17,3 +24,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Define a route for the root URL (/)
+app.get('/ping', async (req, res) => {
+  await pool.query('SELECT NOW()')
+  return res.json(result.rows[0]);
+});
+
+pool.connect()
+  .then(() => console.log('Connected to PostgreSQL database on Render'))
+  .catch(err => console.error('Error connecting to PostgreSQL database', err));
+
+module.exports = client;
