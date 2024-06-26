b059e7029b9e3726772d0b1b4148273725743438
circulomilitarec <sistemas@circulomilitar.ec>
2024-06-26 13:23:20 -0500

    Database settings

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
 // server.js
-
-//const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/restaurante';
-
-//mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const { Pool } = require('pg');
 const express = require('express');
 const app = express();
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Utiliza esto para conexiones SSL. Modifica según sea necesario.
-  }
-})
 const port = process.env.PORT || 3000;
+const client = require('./db'); // Asegúrate de que el cliente de PostgreSQL esté exportado correctamente
 
 // Define a route for the root URL (/)
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await client.query('SELECT NOW()');
+    res.send(result.rows[0]);
+  } catch (err) {
+    console.error('Error executing query', err.stack);
+    res.status(500).send('Error connecting to database');
+  }
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// Define a route for the root URL (/)
-app.get('/ping', async (req, res) => {
-  await pool.query('SELECT NOW()')
-  return res.json(result.rows[0]);
-});
-
-pool.connect()
-  .then(() => console.log('Connected to PostgreSQL database on Render'))
-  .catch(err => console.error('Error connecting to PostgreSQL database', err));
-
-module.exports = pool;
