83f8ff90eaa928360f219c6e5f3def55a31e6ccd
circulomilitarec <sistemas@circulomilitar.ec>
2024-06-27 11:05:22 -0500

    desarrollo 1

--- backend/db.js
+++ backend/db.js
@@ -3,17 +3,6 @@ const { Client } = require('pg');
 const pgp = require('pg-promise')();
 require('dotenv').config(); // Asegúrate de tener esta línea al principio para cargar las variables de entorno
 
-/*
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Utiliza esto para conexiones SSL. Modifica según sea necesario.
-  }
-});
-*/
-
-//const db = pgp(process.env.DATABASE_URL);
-
 const db = pgp({
   connectionString: process.env.DATABASE_URL,
   ssl: {
