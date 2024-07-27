860b258b0a22b181ef8e098f914240fbf4e881c0
Panafae259516 <108027540+roncortez@users.noreply.github.com>
2024-07-27 00:24:43 -0500

    db

--- backend/db.js
+++ backend/db.js
@@ -4,10 +4,13 @@ const pgp = require('pg-promise')();
 require('dotenv').config(); // Asegúrate de tener esta línea al principio para cargar las variables de entorno
 
 const db = pgp({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Configura según la configuración de tu base de datos
-  },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  // Desactiva SSL si no es necesario
+  ssl: false
 });
 
 
